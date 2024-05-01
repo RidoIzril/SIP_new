48927017a06b1034f2d81bce6296ad3d89f34f82
RidoIzril <ridoizril@gmail.com>
2024-05-02 00:48:46 +0700

    modifikasi

---
+++ header.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include("inc_koneksi.php");
+if (!isset($_SESSION['admin_username'])) {
+    header("location:login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="app">
+        <nav>
+            <ul>
+                <li><a href="admin_depan.php">Halaman Depan</a></li>
+                <?php if (in_array("guru", $_SESSION['admin_akses'])) { ?>
+                    <li><a href="admin_guru.php">Halaman Guru</a></li>
+                <?php } ?>
+                <?php if (in_array("customer", $_SESSION['admin_akses'])) { ?>
+                    <li><a href="customer.html">customer</a></li>
+                <?php } ?>
+                <?php if (in_array("kasir", $_SESSION['admin_akses'])) { ?>
+                    <li><a href="">Halaman</a></li>
+                <?php } ?>
+                <li><a href="logout.php">Logout >></a></>
+            </ul>
+        </nav>
